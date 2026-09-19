2ddc0f35a72cc35c049e47da51f93846a1019da7
srujaninvest-blip <srujaninvest@gmail.com>
2026-09-19 08:53:31 +0530

    Fix Express API catch-all routing

--- api/[...path].ts
+++ api/[...path].ts
@@ -1,3 +1,11 @@
-import app from "../server.ts";
+import app from "../server";
 
-export default app;
+export default function handler(req: any, res: any) {
+  const currentUrl = req.url || "/";
+
+  if (!currentUrl.startsWith("/api")) {
+    req.url = `/api${currentUrl.startsWith("/") ? "" : "/"}${currentUrl}`;
+  }
+
+  return app(req, res);
+}
